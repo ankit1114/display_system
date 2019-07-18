fb49f579bbc5a241a9a8461f2267c1d382bdcfef
ankit1114 <46845315+ankit1114@users.noreply.github.com>
2019-07-19 01:14:13 +0530

    Add files via upload

---
+++ EnterBooks.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<center><h2>vikas</h2></center>>
+</body>
+</html>
---
+++ searchbooks.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>search books</title>
+</head>
+<body bgcolor="87ceeb">
+	<center> <h2>Online Library managements System</h2></center>
+	<form action="Displaybooks.php" method="get">
+	<br>  
+    <center>Enter the title of the book to be searched : 
+    	<input type="text" name="search" size="48">
+    	<br></br>
+    	<input type="submit" name="submit">
+    	<input type="reset" name="reset">
+    </center> 
+    <br>
+     </form>
+</body>
+</html>
